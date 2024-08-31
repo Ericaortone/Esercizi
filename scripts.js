4304561cb1d5dcb8cd0d6ced7da21633e86edf90
Ericaortone <152892540+Ericaortone@users.noreply.github.com>
2024-08-31 17:24:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,1 @@
-// Mostra il pulsante quando si scorre oltre 200px
-window.onscroll = function() {
-    let scrollTopButton = document.getElementById("scrollTopButton");
-    
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        // Mostra il pulsante se si è scorsi oltre 200px
-        scrollTopButton.style.display = "block";
-    } else {
-        // Nasconde il pulsante se si è tornati in cima
-        scrollTopButton.style.display = "none";
-    }
-};
 
-// Scorrimento verso l'alto con animazione
-document.getElementById("scrollTopButton").onclick = function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
